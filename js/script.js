f67424b573f43ba92c8c1ced7ef91cef297b36a4
any.okulova <any-okulova@yandex.ru>
2020-01-31 17:44:37 +0300

    add delete li items

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
-function addNewLi() {
-    var text = document.getElementById("inp").value;
-    if (!(text === "undefined" || text === null || text ===  ""))
-    {
+btn_elem.onclick = function() {
+    var inp = document.getElementById("inp");
+    if (!(inp.value === "undefined" || inp.value === null || inp.value ===  "")) {
+        var parent = document.getElementById("list");
+
         var li = document.createElement("li");
-        li.innerText = text;
-        list.append(li);
+        li.innerText = inp.value;
+
+        var del = document.createElement("input");
+        del.type = "button";
+        del.value = "X";
+        del.id = parent.getElementsByTagName('li').length;
+        del.addEventListener('click', () => li.remove());
+
+        li.appendChild(del);
+        parent.appendChild(li);
+
+        inp.value = "";
     }
-}
+};
